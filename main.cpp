a69b82de1416ccb47c5113559def64a9cc779a53
KeyHeart <gerber.kevin@live.fr>
2022-10-30 23:10:36 +0100

    Create grid

--- main.cpp
+++ main.cpp
@@ -1,14 +1,31 @@
-#include <SDL2/SDL.h>
+// Compile: g++ -O2 -lSDL2 -std=c++20 game.cpp
+
 #include <cstdlib>
 #include <iostream>
-#include <time.h>
 #include <vector>
-#include <tuple>
-#include <algorithm>
-#include <memory>
+#include <string.h>
 
 using namespace std;
 
+void createGrid(int gridSize)
+{
+    vector<vector<string>> grid;
+
+    for (int i = 0; i< gridSize; i++)
+    {
+        for (int j = 0; j< gridSize; i++)
+        {
+            grid[i][j] = ".";
+            cout << grid[i][j] << " ";
+
+        }
+        cout << endl;
+    }
+
+}
+
 int main()
 {
+    createGrid(10);
+    return 0;
 }
