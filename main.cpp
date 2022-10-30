7866a4891e9ce45f2d731833b1496a5c6cea8ab9
KeyHeart <gerber.kevin@live.fr>
2022-10-30 19:41:20 +0100

    init main

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <SDL2/SDL.h>
+#include <cstdlib>
+#include <iostream>
+#include <time.h>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+#include <memory>
+
+using namespace std;
+
+int main()
+{
+}
